695b115641d3fb7a5625809aef267e782183ea73
MICHELLEHRBS <106103054+MICHELLEHRBS@users.noreply.github.com>
2024-03-13 01:43:35 -0400

    se cambio las relacion de ventas y productos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,15 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
+
     protected $table = "products";
 
-    public function category () : BelongsTo {
+    public function category () : BelongsTo
+    {
         return $this->belongsTo(Category::class);
     }
 
     protected $fillable = ['category_id'];
+
+
+
+    public function ventas():  BelongsTo
+    {
+        return $this->belongsToMany(venta::class, );
+    }
+
 }
--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Venta extends Model
 {
     use HasFactory;
+
     protected $table = "ventas";
+
+    public function products () : BelongsToMany
+     {
+        return $this->belongsToMany(Category::class);
+    }
 }
